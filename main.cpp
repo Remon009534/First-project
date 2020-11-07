9bb2e71df957755514f961d2f97b49c1b6fef34c
Remon009534 <70149212+Remon009534@users.noreply.github.com>
2020-11-07 12:38:34 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+    char op;
+
+    cout << "Enter a number1: ";
+    cin >> num1;
+
+    cout << "Enter a number2: ";
+    cin >> num2;
+    
+    cout << "Enter a operator: ";
+    cin >> op;
+
+    switch (op)
+    {
+        case "+":
+        cout << num1 + num2;
+        break;
+        case "-":
+        cout << num1 - num2;
+        break;
+        case "*":
+        cout << num1 * num2;
+        break;
+        case "/":
+        cout << num1 / num2;
+        break;
+        default:
+        cout << "Invalid number!!!"
+    }
+}
